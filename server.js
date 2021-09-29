2f84878d3a232f8b03a4fba27407d86d98eb5feb
Brayan Santos <santos.a.brayan97@gmail.com>
2021-09-29 15:02:49 -0400

    chat room connected

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require("express");
+const app = express();
+const {createServer} = require('http');
+
+app.use(express.static('build'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/build/index.html`))
+})
+
+const httpServer = createServer(app);
+const PORT = process.env.PORT || 3000;
+const {Server} = require("socket.io");
+const io = new Server(httpServer);
+
+io.on('connection', function(socket) {
+  socket.on('send', function(message){
+      io.emit('receive', message);
+  });
+});
+httpServer.listen(PORT);
--- src/jamiePages/chat.js
+++ src/jamiePages/chat.js
@@ -1,51 +1,82 @@
 import React from 'react';
-const io = require("socket.io-client");
-const socket = io("https://noetic-talk-socket.herokuapp.com/");
-const messages = this.state.messages;
-constructor(){
-  super();
-  this.state={
-    messages:[]
+import { io } from "socket.io-client";
+const socket = io();
+
+export default class Chat extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      message:'',
+      messages:[]
+    }
+    this.send = this.send.bind(this)
   }
-}
-const send = async() => {
-  socket.emit('send', message)
-}
-socket.on('receive', message){
-  if(messages.length<11){
-    messages.push(message);
+  async send(){
+    await socket.emit('send', this.state.message)
   }
-  else{
-    messages.shift();
-    messages.push(message);
-  }
-}
-render(){
-  return(
-  <div>
-/*    <div id='roomSelector'>
-      <button id='room1'class='roomBtn'>Room 1</button>
-      <button id='room2'class='roomBtn'>Room 2</button>
-      <button id='room3'class='roomBtn'>Room 3</button>
-      <button id='room4'class='roomBtn'>Room 4</button>
-      <button id='room5'class='roomBtn'>Room 5</button>
-      <button id='room6'class='roomBtn'>Room 6</button>
-      </div>*/
-    <div id='messagebox'>
-      <div id='message1' class='message'>{messages[0]}</div>
-      <div id='message2' class='message'>{messages[1]}</div>
-      <div id='message3' class='message'>{messages[2]}</div>
-      <div id='message4' class='message'>{messages[3]}</div>
-      <div id='message5' class='message'>{messages[4]}</div>
-      <div id='message6' class='message'>{messages[5]}</div>
-      <div id='message7' class='message'>{messages[6]}</div>
-      <div id='message8' class='message'>{messages[7]}</div>
-      <div id='message9' class='message'>{messages[8]}</div>
-      <div id='message10' class='message'>{messages[9]}</div>
+  async componentDidMount(){
+    const copyMessages = [...this.state.messages];
+    socket.on('receive', message => {
+          if(this.state.messages.length<10){
+            copyMessages.push(message);
+            this.setState({messages:copyMessages});
+          }
+          else{
+            console.log('made it to the else sta')
+            copyMessages.shift();
+            console.log(copyMessages)
+            copyMessages.push(message);
+            console.log(copyMessages)
+            this.setState({messages:copyMessages});
+          }
+        })
+    }
+    // async componentDidUpdate(){
+    //   const copyMessages = [...this.state.messages];
+    //   socket.on('receive', message => {
+    //         if(this.state.messages.length<11){
+    //           copyMessages.push(message);
+    //           this.setState({messages:copyMessages});
+    //         }
+    //         else{
+    //           console.log('made it to the else sta')
+    //           copyMessages.shift();
+    //           console.log(copyMessages)
+    //           copyMessages.push(message);
+    //           console.log(copyMessages)
+    //           this.setState({messages:copyMessages});
+    //         }
+    //       })
+    // }
+  render(){
+    return(
+    <div className="ChatComponent">
+      <div id='roomSelector'>
+        <button id='room1'className='roomBtn'>Room 1</button>
+        <button id='room2'className='roomBtn'>Room 2</button>
+        <button id='room3'className='roomBtn'>Room 3</button>
+        <button id='room4'className='roomBtn'>Room 4</button>
+        <button id='room5'className='roomBtn'>Room 5</button>
+        <button id='room6'className='roomBtn'>Room 6</button>
+      </div>
+      <div id='messagebox'>
+        <div id='message1' className='message'>{this.state.messages[0]}</div>
+        <div id='message2' className='message'>{this.state.messages[1]}</div>
+        <div id='message3' className='message'>{this.state.messages[2]}</div>
+        <div id='message4' className='message'>{this.state.messages[3]}</div>
+        <div id='message5' className='message'>{this.state.messages[4]}</div>
+        <div id='message6' className='message'>{this.state.messages[5]}</div>
+        <div id='message7' className='message'>{this.state.messages[6]}</div>
+        <div id='message8' className='message'>{this.state.messages[7]}</div>
+        <div id='message9' className='message'>{this.state.messages[8]}</div>
+        <div id='message10' className='message'>{this.state.messages[9]}</div>
+      </div>
+      <div className='textInput'>
+        <input value={this.state.message} onChange={e => {
+          this.setState({message:e.target.value})
+        }}/>
+        <button onClick={(e) => this.send()}>Send</button>
+      </div>
     </div>
-    <div class='textInput'>
-      <input value={message} onChange={e => setMessage(e.target.value)}/>
-      <button onClick={send}>Send</button>
-    </div>
-  </div>
-)}
+  )}
+}
